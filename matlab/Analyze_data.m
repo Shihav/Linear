7ce5d8d81313b2b1f49f1284ce79a935b7a7e715
ASM Shihavuddin <shihav407@yahoo.com>
2018-07-19 16:13:50 +0200

    Add files via upload

--- matlab/Analyze_data.m
+++ matlab/Analyze_data.m
@@ -1,4 +1,7 @@
-lookfor='China';
+%% define search key word or expression
+lookfor='Russia';
+
+%% preprocess search key word or expression
 swordall = preprocessWeatherNarratives(lookfor);
 sword=swordall.Vocabulary;
 
@@ -20,54 +23,46 @@ words = sword;
     
     idx=find(mean(dmat,2)<1.9);
     
-%     data.URL(idx)
-    
-    
+%% Preprocessing articles titles  
     desk = preprocessWeatherNarratives(data.Description);
-desk(1:5)
 
-
-
-deskLengths = doclength(desk);
+% deskLengths = doclength(desk);
 % figure
 % histogram(deskLengths)
-title("Document Lengths")
-xlabel("Length")
-ylabel("Number of Documents")
+% title("Document Lengths")
+% xlabel("Length")
+% ylabel("Number of Documents")
 
+%% Limiting text size in each articles tilte
 sequenceLength = 30;
 deskTruncatedTrain = docfun(@(words) words(1:min(sequenceLength,end)),desk);
 
+%% Convertice articles title to numbers
 deskTrain = doc2sequence(emb,deskTruncatedTrain);
 
     dmat2=[];
     for kin=1:numel(deskTrain)
         doc=deskTrain{kin};
         dv=pdist2(C',doc');
-%         mean(dv,2)
         dvs=sort(dv,2);
-%         mean(dv,2)
-        dmat2(kin,:)= mean(dvs(:,1:round(.3*size(dv,2))),2)';   
-%         dmat2(kin,:)= mean(dv,2)';        
+        dmat2(kin,:)= mean(dvs(:,1:round(.3*size(dv,2))),2)';          
     end
     
     idx2=find(min(dmat2,[],2)<1.8);
-    
-%     data.URL(idx2)
-    
-    
-    
+
+%% Removing duplicate articles   
 %     data.Description
     [C,IA,IC] = unique(data.Description,'stable')
 dataF=data(IA,:);
 dmat2=dmat2(IA,:);
 dmat=dmat(IA,:);
 % score=(min(dmat2,[],2)*.5+min(dmat,[],2)*.5);
+
+%% Final distance score based on titles and description
 score=(mean(dmat2,2)*.55+mean(dmat,2)*.45);
 
+%% Keeping articles less than a threshold
 idx3=find(score<=2.15);
-    
-    unique(dataF.URL(idx3))
-    
-%     unique(dataF.Description(idx3))
-close all
+
+%% Printing selected articles URL
+unique(dataF.URL(idx3))
--- matlab/Process_data.m
+++ matlab/Process_data.m
@@ -12,76 +12,79 @@ clc
 %     load(filename + '.mat')
 % end
 
+%% loading default word to vector table
 emb=fastTextWordEmbedding;
-v_king = word2vec(emb,'king')';
-q_queen = word2vec(emb,'queen')';
-q_coffee = word2vec(emb,'coffee')';
-whos v_king
+% v_king = word2vec(emb,'king')';
+% q_queen = word2vec(emb,'queen')';
+% q_coffee = word2vec(emb,'coffee')';
+% whos v_king
 
+%% Reading the save articles
 src='C:\ENS\the-news-website\Data';
 out1=rdir([src '\**\*.xlsx']);
-% out1(10)=[];
+
 data=[];
 for kin=1:numel(out1)
-
-
 filename = out1(kin).name;
 datat = readtable(filename,'TextType','string');
 data=[data;datat];
 end
 head(data)
 
+%% Removing duplicate articles
 [C,IA,IC] = unique(data.URL,'stable')
 data=data(IA,:);
-
-
 idx = strlength(data.URL) == 0;
 data(idx,:) = [];
-data.Section = categorical(data.Section);
-figure
-h = histogram(data.Section);
-xlabel("Class")
-ylabel("Frequency")
-title("Class Distribution")
-data.Description
 
+% data.Section = categorical(data.Section);
+% figure
+% h = histogram(data.Section);
+% xlabel("Class")
+% ylabel("Frequency")
+% title("Class Distribution")
+% data.Description
+
+%% Preprocessing articles descriptions
 documents = preprocessWeatherNarratives(data.Text);
-documents(1:5)
+% documents(1:5)
 
-bag = bagOfNgrams(documents)
+% bag = bagOfNgrams(documents)
+% 
+% figure
+% wordcloud(bag);
+% title("Weather Reports: Preprocessed Bigrams")
+% 
+% mdl = fitlda(bag,10);
+% figure
+% for i = 1:4
+%     subplot(2,2,i)
+%     wordcloud(mdl,i);
+%     title("LDA Topic " + i)
+% end
+% 
+% cleanTextData = erasePunctuation(data.Text);
+% documents2 = tokenizedDocument(cleanTextData);
+% 
+% bag = bagOfNgrams(documents2,'NGramLengths',3);
+% 
+% figure
+% wordcloud(bag);
+% title("Weather Reports: Trigrams")
+% 
+% tbl = topkngrams(bag,10)
+% 
+% 
+% documentLengths = doclength(documents);
+% figure
+% histogram(documentLengths)
+% title("Document Lengths")
+% xlabel("Length")
+% ylabel("Number of Documents")
 
-figure
-wordcloud(bag);
-title("Weather Reports: Preprocessed Bigrams")
-
-mdl = fitlda(bag,10);
-figure
-for i = 1:4
-    subplot(2,2,i)
-    wordcloud(mdl,i);
-    title("LDA Topic " + i)
-end
-
-cleanTextData = erasePunctuation(data.Text);
-documents2 = tokenizedDocument(cleanTextData);
-
-bag = bagOfNgrams(documents2,'NGramLengths',3);
-
-figure
-wordcloud(bag);
-title("Weather Reports: Trigrams")
-
-tbl = topkngrams(bag,10)
-
-
-documentLengths = doclength(documents);
-figure
-histogram(documentLengths)
-title("Document Lengths")
-xlabel("Length")
-ylabel("Number of Documents")
-
+%% Limiting text size in each articles
 sequenceLength = 500;
 documentsTruncatedTrain = docfun(@(words) words(1:min(sequenceLength,end)),documents);
 
+%% Convertice articles to numbers
 XTrain = doc2sequence(emb,documentsTruncatedTrain);
