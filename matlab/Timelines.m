9ebfdb89cec029a8ee87aab6324b79225a6c39ec
ASM Shihavuddin <shihav407@yahoo.com>
2018-08-13 17:19:41 +0200

    Add files via upload

--- matlab/Timelines.m
+++ matlab/Timelines.m
@@ -1,9 +1,13 @@
 clear all;close all;clc
 % 
 % %% loading default word to vector table
+
+%% We need to have a word to vector conversion table to load (which is normally learned by deep learning analyzing wiki pidia and twitter at Glove website).
+%% In matlab they have a default one much lighter. Which we are now loading 
+%% loading default word to vector table
 emb=fastTextWordEmbedding;
 warning('OFF', 'MATLAB:table:ModifiedAndSavedVarnames')
-%% Reading the save articles
+%% Reading the save articles (Loading some samples articles)
 srcin='..\Data';
 out1=rdir([srcin '\**\*.xlsx']);
 data=[];
@@ -20,37 +24,50 @@ data=data(IA,:);
 idx = strlength(data.URL) == 0;
 data(idx,:) = [];
 
+%% Here we will initially analyze 95% of the text to generate the already formed dataset. and 5% to look for any new potential timelines
 rndidx=randperm(size(data,1));
 mainid=rndidx(1:round(length(rndidx)*.95));
 newid=rndidx(round(length(rndidx)*.95)+1:end);
 
 ndata=data;
-data=data(mainid,:);
-ndata=ndata(newid,:);
+data=data(mainid,:); %old data
+ndata=ndata(newid,:); %new data
 
 %% Preprocessing articles descriptions
+% this step included prepressing the documents for removing stop words,
+% prepositions, punctuations etc.
 documents = preprocessWeatherNarratives(data.Text);
 
 %% Limiting text size in each articles
+% we could limit the main text of the atricles to a certain length if it is
+% too big. These are not really necessary to
+% do
 sequenceLength = 1500;
 documentsTruncatedTrain = docfun(@(words) words(1:min(sequenceLength,end)),documents);
 
 %% Convertice articles to numbers
+% now we convert the whole text of each articles into numbers
 XTrain = doc2sequence(emb,documentsTruncatedTrain);
-clc
 
 %% Preprocessing articles titles  
+% The word Description is actually used to represent titles here. It is a
+% bit confusing sometimes.
     desk = preprocessWeatherNarratives(data.Description);
 
 %% Limiting text size in each articles tilte
+% we limit the title to maximum 50 words. These are not really necessary to
+% do
 sequenceLength = 50;
 deskTruncatedTrain = docfun(@(words) words(1:min(sequenceLength,end)),desk);
 
 %% Convertice articles title to numbers
 deskTrain = doc2sequence(emb,deskTruncatedTrain);
 
+%% we keep the unprocessed words of the title to make the timelines look more realistic
 Titles = preprocessWeatherNarratives(data.Description);
 %Titles = deskTrain;
+
+%% We are looking for 2 word or 3 word phrases that are unique and more frequent that normalized threshold values
 
 bag2 = bagOfNgrams(Titles,'NgramLengths',2);
 bag2 = removeInfrequentNgrams(bag2,ceil(0.02*size(Titles,1)/2)+1);
@@ -78,15 +95,18 @@ for kin=1:size(val3.Ngram,1)
     kin2=kin2+1;
 end
 
+%% Making the final list of timeline expressions
 %% define search key word or expression
 for sc=1:kin2-1
 lookfor=Alookfor{sc,1}
+
+%% for each of the found times now will find the relavent texts from the old database
 %% preprocess search key word or expression
 swordall = preprocessWeatherNarratives(lookfor);
 sword=swordall.Vocabulary;
 np=numel(sword);
 words = sword;
-    idx = ~ismember(emb,words);
+    idx = ~ismember(emb,words); %% here we remove the words that is not in the conversion dictionary
     words(idx) = [];
     C = word2vec(emb,words)';
     
@@ -94,19 +114,19 @@ words = sword;
     dmat=[];
     for kin=1:numel(XTrain)
         doc=XTrain{kin};
-        dv=pdist2(C',doc');
+        dv=pdist2(C',doc'); % findiing the distance from each articles to the search key words
         dvs=sort(dv,2);
 %         mean(dv,2)
-        dmat(kin,:)= mean(dvs(:,1:round(.25*size(dv,2))),2)';        
+        dmat(kin,:)= mean(dvs(:,1:round(.25*size(dv,2))),2)';    % best 25% similar word distance is taken and the mean of the,m is the final output    
     end    
 
-%% distance from title    
+%% distance from title is done in the similar way ike the main text 
 dmat2=[];
     for kin=1:numel(deskTrain)
         doc=deskTrain{kin};
         dv=pdist2(C',doc');
         dvs=sort(dv,2);
-        dmat2(kin,:)= mean(dvs(:,1:round(.3*size(dv,2))),2)';          
+        dmat2(kin,:)= mean(dvs(:,1:round(.3*size(dv,2))),2)'; % best 30% similar word distance is taken and the mean of the,m is the final output           
     end    
 
 %% Removing duplicate articles   
@@ -152,16 +172,12 @@ ndeskTruncatedTrain = docfun(@(words) words(1:min(sequenceLength,end)),ndesk);
 %% Convertice articles title to numbers
 ndeskTrain = doc2sequence(emb,ndeskTruncatedTrain);
 
+%% Everything now similarly done of the new 5% articles as described ourlier in a normalized fashion, so it should give any possible new timeline if occurs.
 nTitles = preprocessWeatherNarratives(ndata.Description);
-%Titles = deskTrain;
 
 nbag2 = bagOfNgrams(nTitles,'NgramLengths',2);
 nbag2 = removeInfrequentNgrams(nbag2,ceil(0.02*size(nTitles,1)/2)+1);
 nval2 = topkngrams(nbag2, size(nbag2.Ngrams,1)+1)
-
-% bag1 = bagOfNgrams(Titles,'NgramLengths',1);
-% bag1 = removeInfrequentNgrams(bag1,ceil(0.02*size(Titles,1)/.5));
-% topkngrams(bag1, size(bag1.Ngrams,1))
 
 nbag3 = bagOfNgrams(nTitles,'NgramLengths',3);
 nbag3 = removeInfrequentNgrams(nbag3,ceil(0.02*size(nTitles,1)/4)+1);
